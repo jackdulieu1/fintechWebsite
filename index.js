65cfc6a4a9b6646ae093d4630a5713b6546db67f
jackdulieu1 <61517434+jackdulieu1@users.noreply.github.com>
2021-05-20 12:18:31 +0100

    Added initial files

---
+++ index.js
@@ -0,0 +1,16 @@
+function changeImage(){
+
+  var image = document.getElementById('myImage');
+
+  if
+
+  (image.src.match("images/Bitcoin_Cash.png"))
+
+  {image.src="images/Bitcoin_SV.png";}
+
+  else
+  {
+    {image.src="images/Bitcoin_Cash.png";}
+  }
+
+}
